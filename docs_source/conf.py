cfe6f8edeb18eb73bfeeaf3a755385e04134c9ee
joran <joran@decagon.com>
2017-04-09 22:14:58 -0700

    removed image

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -29,6 +29,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import os
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
@@ -37,7 +39,9 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
 ]
-
+import sys
+sys.path.insert(0,os.path.abspath(".."))
+os.environ['PYTHONPATH'] = os.path.abspath("..")
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
